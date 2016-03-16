3d200bcebe711b014c966486c329ff087e278aba
、Campanulaceae <932191671@qq.com>
2016-03-16 12:42:34 +0800

    add 一致性Hash

---
+++ src/main/java/com/tinymood/dsandalgorithm/StrngHashCodeTest.java
@@ -0,0 +1,11 @@
+package com.tinymood.dsandalgorithm;
+
+public class StrngHashCodeTest {
+	public static void main(String[] args) {
+		System.out.println("192.168.0.0:111的哈希值：" + "192.168.0.0:1111".hashCode());
+		System.out.println("192.168.0.1:111的哈希值：" + "192.168.0.1:1111".hashCode());
+		System.out.println("192.168.0.2:111的哈希值：" + "192.168.0.2:1111".hashCode());
+		System.out.println("192.168.0.3:111的哈希值：" + "192.168.0.3:1111".hashCode());
+		System.out.println("192.168.0.4:111的哈希值：" + "192.168.0.4:1111".hashCode());
+	}
+}
