6e42cbccbd258b8e5471bf12da87db9e53ee869c
、Campanulaceae <932191671@qq.com>
2016-03-23 19:00:57 +0800

    fix

--- src/main/java/com/tinymood/sort/HalfSearch.java
+++ src/main/java/com/tinymood/sort/HalfSearch.java
@@ -1,5 +1,7 @@
 package com.tinymood.sort;
 
+import java.util.Arrays;
+
 /**
  * 折半查找
  * @author 哓哓
@@ -17,5 +19,11 @@ public class HalfSearch {
 				return mid;
 		}
 		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5,6,7};
+		System.out.println(Arrays.binarySearch(a, 5));
+		System.out.println(new HalfSearch().halfSearch(a, 0, a.length-1, 5));
 	}
 }
