309af1f29181e22ec833376438cde571d9724495
、Campanulaceae <932191671@qq.com>
2017-04-02 18:52:38 +0800

    update

--- src/main/java/com/ryan/util/BatRenameFilename.java
+++ src/main/java/com/ryan/util/BatRenameFilename.java
@@ -3,54 +3,58 @@ package com.ryan.util;
 import java.io.File;
 
 public class BatRenameFilename {
-	// public static void main(String[] args) {
-	// File srcFolder = new File("D:\\New");
-	//
-	// File[] fileStr = srcFolder.listFiles();
-	// for (File f: fileStr) {
-	// if (f.isFile()) {
-	// if (f.getName().endsWith(".jpg")) {
-	// System.out.println(f.getName());
-	// }
-	// }
-	// }
-	// }
+    // public static void main(String[] args) {
+    // File srcFolder = new File("D:\\New");
+    //
+    // File[] fileStr = srcFolder.listFiles();
+    // for (File f: fileStr) {
+    // if (f.isFile()) {
+    // if (f.getName().endsWith(".jpg")) {
+    // System.out.println(f.getName());
+    // }
+    // }
+    // }
+    // }
 
-	// 文件名称过滤器
-	// public static void main(String[] args) {
-	// File srcFolder = new File("D:\\New");
-	// String[] strArray = srcFolder.list(new FilenameFilter() {
-	//
-	// @Override
-	// public boolean accept(File dir, String name) {
-	// File f = new File(dir, name);
-	//
-	// return f.isFile() && f.getName().endsWith(".jpg");
-	// }
-	// });
-	//
-	// for (String s : strArray) {
-	// System.out.println(s);
-	// }
-	// }
-	// Demo 批量修改本地文件名
+    // 文件名称过滤器
+    // public static void main(String[] args) {
+    // File srcFolder = new File("D:\\New");
+    // String[] strArray = srcFolder.list(new FilenameFilter() {
+    //
+    // @Override
+    // public boolean accept(File dir, String name) {
+    // File f = new File(dir, name);
+    //
+    // return f.isFile() && f.getName().endsWith(".jpg");
+    // }
+    // });
+    //
+    // for (String s : strArray) {
+    // System.out.println(s);
+    // }
+    // }
+    // Demo 批量修改本地文件名
 
-	public static void main(String[] args) {
-		File srcFolder = new File("D:\\New1");
-		File desFolder = new File("D:\\New3");
+    public static void main(String[] args) {
+        File srcFolder = new File("C:\\Users\\Ryan Tao\\Desktop\\dataset2");
+        File desFolder = new File("C:\\Users\\Ryan Tao\\Desktop\\newdataset2");
 
-		File[] fileArray = srcFolder.listFiles();
+        File[] fileArray = srcFolder.listFiles();
 
-		int i = 1; 
-		for (File f:fileArray) {
-			if (f.isFile() && (f.getName().endsWith(".md"))) {
-				String newName = i + ".md";
-				File newFile = new File(desFolder, newName);
-				f.renameTo(newFile);
-				i++;
-				System.out.println(i + "--" + newFile.getName());
-			}
-		}
+        int i = 1;
+        for (File f : fileArray) {
+            if (f.isFile()) {
+                int suffixIndex = f.getName().lastIndexOf(".");
+                String suffix = f.getName().substring(suffixIndex, f.getName().length());
+                String newName = i + suffix;
+                File newFile = new File(desFolder, newName);
+                f.renameTo(newFile);
+                i++;
+                System.out.println(i + "--" + newFile.getName());
+            } else {
+                //recursive
+            }
+        }
 
-	}
+    }
 }
